9cde95d9d06c618f72e11799b0bf89e25d069490
MajikalExplosions <31389551+MajikalExplosions@users.noreply.github.com>
2019-10-25 19:06:46 -0700

    Add cargo subsystem

--- 2019-offseason/src/main/java/frc/robot/Constants.java
+++ 2019-offseason/src/main/java/frc/robot/Constants.java
@@ -1,5 +1,7 @@
 package frc.robot;
 
 public final class Constants {
-
+    public static int PORT_JOYSTICK_DRIVE = 0;
+    public static int PORT_JOYSTICK_OPERATOR = 1;
+    
 }
--- 2019-offseason/src/main/java/frc/robot/RobotContainer.java
+++ 2019-offseason/src/main/java/frc/robot/RobotContainer.java
@@ -1,6 +1,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
+import frc.robot.JoystickF310.ButtonF310;
 import frc.robot.commands.ExtendClimbCommand;
 import frc.robot.commands.ExtendHatchBeakCommand;
 import frc.robot.commands.ExtendHatchCommand;
@@ -17,33 +18,39 @@ import edu.wpi.first.wpilibj2.command.Command;
 
 public class RobotContainer {
 
+    private final JoystickF310 joystickDrive = new JoystickF310(Constants.PORT_JOYSTICK_DRIVE);
+    private final JoystickF310 joystickOperator = new JoystickF310(Constants.PORT_JOYSTICK_OPERATOR);
+
     private final HatchSubsystem m_hatch = new HatchSubsystem();
     private final HatchBeakSubsystem m_hatchBeak = new HatchBeakSubsystem();
+    private final TurretSubsystem m_turret = new TurretSubsystem();
     private final ClimbSubsystem m_climbFront = new ClimbSubsystem();
     private final ClimbSubsystem m_climbRear = new ClimbSubsystem();
-    private final TurretSubsystem m_turret = new TurretSubsystem();
     private final DrivetrainSubsystem m_drivetrain = new DrivetrainSubsystem();
 
-    
+
     private final ExtendHatchCommand m_extendHatchCommand = new ExtendHatchCommand(m_hatch);
     private final RetractHatchCommand m_retractHatchCommand = new RetractHatchCommand(m_hatch);
     
     private final ExtendHatchBeakCommand m_extendHatchBeakCommand = new ExtendHatchBeakCommand(m_hatchBeak);
     private final RetractHatchBeakCommand m_retractHatchBeakCommand = new RetractHatchBeakCommand(m_hatchBeak);
 
+    private final TurnTurretCommand m_turnTurret = new TurnTurretCommand(m_turret);
+
     private final ExtendClimbCommand m_extendClimbFrontCommand = new ExtendClimbCommand(m_climbFront);
     private final RetractClimbCommand m_retractClimbFrontCommand = new RetractClimbCommand(m_climbRear);
     private final ExtendClimbCommand m_extendClimbRearCommand = new ExtendClimbCommand(m_climbFront);
     private final RetractClimbCommand m_retractClimbRearCommand = new RetractClimbCommand(m_climbRear);
     
-    private final TurnTurretCommand m_turnTurret = new TurnTurretCommand(m_turret);
 
 
     public RobotContainer() {
         configureButtonBindings();
     }
 
-    private void configureButtonBindings() {}
+    private void configureButtonBindings() {
+        joystickOperator.getJoystickButton(ButtonF310.BumperLeft).whenActive(m_extendHatchCommand);
+    }
 
     public Command getAutonomousCommand() {
         return null;
